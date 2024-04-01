c0c0cb95c6a70982846d9166bbc0f8f0344926da
Abhijeet Bisen <abhijeetbisen8@gmail.com>
2024-04-01 23:59:51 +0530

    first commit

---
+++ 12.3.1 Random Number Generation In.js
@@ -0,0 +1,19 @@
+
+// var n = Math.random();
+// n *= 6;
+// n = Math.floor(n) + 1;
+// console.log(n);
+
+
+
+
+// Building Love Calculator
+
+console.log("My name is Abhijeet"); // If we want to take user input so we have to write prompt instead of console.log()
+console.log("Her name is Ellyse");
+var loveScore = Math.random();
+loveScore *= 100;
+loveScore = Math.floor(loveScore) + 1;
+console.log("Your love score is " + loveScore); // To display this we will use alert instead of console.log()
+
+
